39dec97856a111f780239930e4929123fd2d322d
Syoumzic <rofetofe@mail.ru>
2023-11-03 00:37:51 +0500

    исправление ошибок связанных с определением даты

--- src/main/java/botLogic/Calendar.java
+++ src/main/java/botLogic/Calendar.java
@@ -2,18 +2,29 @@ package botLogic;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
 
 public class Calendar {
     /**
      * возвращает сколько дней прошло с прошлой чётной недели первого дня относительно текущей даты
+     * @param string дата
      * @return смещенная дата
      */
-    public int getFirstDayOfEvenWeek(){
-        return getFirstDayOfEvenWeek(LocalDate.now());
-    }
+    public int getFirstDayOfEvenWeek(String string) throws DateTimeParseException {
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .appendPattern("dd.MM")
+                .parseDefaulting(ChronoField.YEAR, LocalDate.now().getYear())
+                .toFormatter(Locale.US);
 
+        LocalDate date = LocalDate.parse(string, formatter);
+        return getFirstDayOfEvenWeek(date);
+    }
     /**
      * возвращает сколько дней прошло с прошлой чётной недели первого дня относительно даты
      * @param date дата
@@ -27,7 +38,7 @@ public class Calendar {
         if (shiftDate.getDayOfWeek() == DayOfWeek.SUNDAY) //неделя в России начинается не с воскресенья
             weekOfYear -= 1;
 
-        if (weekOfYear % 2 == 0)shiftDate = shiftDate.with(ChronoField.DAY_OF_WEEK, 1);
+        if (weekOfYear % 2 == 0) shiftDate = shiftDate.with(ChronoField.DAY_OF_WEEK, 1);
         else shiftDate = shiftDate.minusWeeks(1).with(ChronoField.DAY_OF_WEEK, 1);
 
         return (int)shiftDate.until(date, ChronoUnit.DAYS);
--- src/main/java/botLogic/parameterHandler/DateHandler.java
+++ src/main/java/botLogic/parameterHandler/DateHandler.java
@@ -9,56 +9,59 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 
 public class DateHandler implements ParameterHandler{
     public String startMessage(){
-        return "Укажите день";
+        return "Укажите день (например 01.12)";
     }
 
     public String action(User user, String message){
-        if(dateIsCorrect(message)){
-            user.flushParameterHandler();
-            Calendar calendar = new Calendar();
-            int numberDay = calendar.getFirstDayOfEvenWeek() + 1;
-
-            List<String> schedule = null;
-            try{
-                schedule = user.getDatabase().getSchedule(user.getId(), numberDay);
-            } catch (SQLException ex) {
-                int errNum = ex.getErrorCode();
-                if (errNum == 1146){
-                    try {
-                        List<List<String>> schedule_pars = user
-                                .getWebParser()
-                                .parse(user.getDatabase()
-                                        .getUsersGroup(user.getId())
-                                        .toUpperCase());
-                        user
-                            .getDatabase()
-                            .setSchedule(user.getDatabase().getUsersGroup(user.getId()), schedule_pars);
-                        schedule = user.getDatabase().getSchedule(user.getId(), numberDay);
-
-                    } catch (SQLException e) {
-                        System.out.println(e.getMessage());
-                    } catch (ParseException e){
-                        return "ошибка считывания расписания. Попробуйте позже";
-                    }catch (IOException e){
-                        System.out.println("ошибка соединения с интернетом");
-                    }
-                }
-                else {
-                    System.out.println(ex.getMessage());
-                }
-            }
-
-            assert schedule != null;
-
-            return toString(schedule);
+        Calendar calendar = new Calendar();
+        int numberDay;
+        try {
+            numberDay = calendar.getFirstDayOfEvenWeek(message);
+        } catch (DateTimeParseException e) {
+            return "введена некоректная дата";
         }
 
-        return "дата введена некоректно!";
+        List<String> schedule = null;
+        try{
+            schedule = user.getDatabase().getSchedule(user.getId(), numberDay + 1);
+        } catch (SQLException ex) {
+            int errNum = ex.getErrorCode();
+            if (errNum == 1146){
+                try {
+                    List<List<String>> schedule_pars = user
+                            .getWebParser()
+                            .parse(user.getDatabase()
+                                    .getUsersGroup(user.getId())
+                                    .toUpperCase());
+                    user
+                        .getDatabase()
+                        .setSchedule(user.getDatabase().getUsersGroup(user.getId()), schedule_pars);
+                    schedule = user.getDatabase().getSchedule(user.getId(), numberDay);
+
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                } catch (ParseException e){
+                    return "ошибка считывания расписания. Попробуйте позже";
+                }catch (IOException e){
+                    System.out.println("ошибка соединения с интернетом");
+                }
+            }
+            else {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        assert schedule != null;
+        user.flushParameterHandler();
+
+        return toString(schedule);
     }
 
     String toString(List<String>schedule){
@@ -69,15 +72,7 @@ public class DateHandler implements ParameterHandler{
     }
 
     private boolean dateIsCorrect(String message){
-        final String dateFormat = "dd.MM";
 
-        DateFormat sdf = new SimpleDateFormat(dateFormat);
-        sdf.setLenient(false);
-        try {
-            sdf.parse(message);
-        } catch (java.text.ParseException e) {
-            return false;
-        }
 
         return true;
     }
